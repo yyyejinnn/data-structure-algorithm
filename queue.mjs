9035d6551550e591be0698200bdbe9efcbf607cd
yyyejinnn <lyjin2007@gmail.com>
2024-04-16 14:28:30 +0900

    queue:

---
+++ queue.mjs
@@ -0,0 +1,40 @@
+/**
+ * -> 기존 연결리스트를 이중연결리스트 자료구조로 변경 필요
+ *
+ * 큐의 추상자료형
+ * enqueue
+ * dequeue
+ * front
+ * isEmpty
+ */
+
+import { DoublyLinkedList } from './doubly-linked-list.mjs';
+
+class Queue {
+  constructor() {
+    this.list = new DoublyLinkedList();
+  }
+
+  enqueue(data) {
+    this.list.insertAt(0, data);
+  }
+
+  dequeue() {
+    try {
+      return this.list.deleteLast();
+    } catch (err) {
+      return null;
+    }
+  }
+
+  front() {
+    // 다음에 제거될 요소(stack의 peek과 유사)
+    return this.list.tail;
+  }
+
+  isEmpty() {
+    return this.list.count == 0;
+  }
+}
+
+export { Queue };
