72fd9448dc7237577de17e909731b48eb50f9377
yyyejinnn <lyjin2007@gmail.com>
2024-04-19 16:28:01 +0900

    bubbleSort:

---
+++ 섹션2. 알고리즘/3.bubble-sort.mjs
@@ -0,0 +1,28 @@
+/**
+ * 버블 정렬
+ * - O(n2) (for문 중첩)
+ * - 이해와 구현이 간단하다.
+ *
+ */
+
+const array = [8, 9, 4, 6, 2];
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        continue;
+      }
+
+      let temp;
+
+      temp = arr[j + 1];
+      arr[j + 1] = arr[j];
+      arr[j] = temp;
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort(array));
